bf5101c6fbaa42b0abfb7ad9550dc210d8c72939
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-02-21 10:04:02 +0100

    first commit

---
+++ contract-backend/contracts/IStakedPunk.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface StakedPunks {
+    /**
+     * @dev returns a token ID by `owner` from a given `index` of its token list
+     */
+    function tokenOfOwnerbyIndex(
+        address owner,
+        uint256 index
+    ) external view returns (uint256 tokenId);
+
+    /**
+     * @dev returns number of tokens in `owner's` account
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+}
